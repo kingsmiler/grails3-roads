c63b87a799d20038cefdc9fba77e1c15024af7fb
kingsmiler <kingsiler@163.com>
2016-02-22 22:41:34 +0800

    upload spring boot startup, with groovy and gradle

---
+++ springboot-startup/src/main/groovy/org/amc/spring/SpringbootStartupApplication.groovy
@@ -0,0 +1,12 @@
+package org.amc.spring
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class SpringbootStartupApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run SpringbootStartupApplication, args
+	}
+}
